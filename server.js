727e2243b885ed11527ca2647513116fccdc44c9
carmelngoyi <carmelkn14@gmail.com>
2025-11-13 10:12:26 +0200

    added aws link

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 app.use(cors({
   origin: [
     "http://localhost:5173",
+    "http://www.cc-goodies.com.s3-website-us-east-1.amazonaws.com/",
     "https://cc-goodies-frontend.vercel.app",
     "https://cc-goodies-frontend-rh475v9rb-carmel-ngoyis-projects.vercel.app"
   ],
